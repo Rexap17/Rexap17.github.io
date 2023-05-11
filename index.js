509f827bf8bcd7ff9d53da2b96434c57a586d223
Isaac Joynes <a1827396@student.adelaide.edu.au>
2023-05-11 08:57:28 +0000

    fgfsgsdfg

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+/
 const vueinst = new Vue({
-    el: "#homePage",
-    data: {
-      top_menu: [{ title:'Home', url:'index.html' },
-      { title:'Events',url:'/events' },
-      { title:'Settings',url:'user.html'}],
-      right_menu: [{title:'Sign Up', url:'/login'}]
+  el: "#homePage",
+  data: {
+    top_menu: [{ title:'Home', url:'index.html' },
+    { title:'Events',url:'/events' },
+    { title:'Settings',url:'user.html'}],
+    right_menu: [{title:'Sign Up', url:'/login'}]
 
-    },
+  },
 });
-
 const bannerContainer = document.querySelector('.eventBannerContainer');
 const scrollLeftBtn = document.querySelector('.scrollLeft');
 const scrollRightBtn = document.querySelector('.scrollRight');
@@ -25,4 +25,10 @@ scrollRightBtn.addEventListener('click', () => {
     left: 300,
     behavior: 'smooth'
   });
+});
+
+
+document.getElementById("submitButton").addEventListener("click", function(event) {
+  event.preventDefault(); // prevent the form from submitting normally
+  window.location.href = "index.html"; // redirect to the index.html page
 });
